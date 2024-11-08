479d6121537d7e92870f85240cc0778bfb429f78
Aluno <Aluno@10.174.12.50>
2024-11-07 21:08:55 -0300

    Criando conexão com o banco

---
+++ gf21_jdbc/src/br/com/caelum/jdbc/TestaExercicio.java
@@ -0,0 +1,17 @@
+package br.com.caelum.jdbc;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class TestaExercicio {
+	
+	
+	public static void main(String[] args) throws SQLException { 
+		Connection connection = new ConnectionFactory().getConnection();
+		System.out.println("Conexao Aberta!");          
+		connection.close();
+	}
+	
+	
+
+}
